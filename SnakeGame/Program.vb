066be58f68b843ea1f1f8504732b110a87ca2c41
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 03:19:08 +0330

    - Reboot Project (Snake Game) - Trying to be more efficient - Significant increase in objectivity

--- SnakeGame/Program.vb
+++ SnakeGame/Program.vb
@@ -5,71 +5,9 @@ Imports System.Threading
 Imports System.Threading.Tasks
 
 Public Module Program
-    Public Property Width As Integer
-    Public Property Height As Integer
-
-    Public Property x As Integer
-    Public Property y As Integer
-
-    Public Property X_Position As Integer
-    Public Property Y_Position As Integer
-
-    Dim info As System.ConsoleKeyInfo = New System.ConsoleKeyInfo()
-    Dim key As Char = "w"
 
     Public Sub Main()
-        Dim finished As Boolean = False
-        Width = 100
-        Height = 50
-        x = 20
-        y = 20
-        System.Console.CursorVisible = False
-        While Not finished
-            DrawBoard()
-            DrawSnake()
-            System.Console.Read()
-        End While
 
-    End Sub
-
-    Public Sub DrawBoard()
-        System.Console.Clear()
-
-        For i As Integer = 0 To Width - 1
-            System.Console.SetCursorPosition(i, 0)
-            System.Console.Write("-")
-        Next
-
-        For i As Integer = 0 To Width - 1
-            System.Console.SetCursorPosition(i, Height)
-            System.Console.Write("-")
-        Next
-
-        For i As Integer = 0 To Height - 1
-            System.Console.SetCursorPosition(0, i)
-            System.Console.Write("-")
-        Next
-
-        For i As Integer = 0 To Height - 1
-            System.Console.SetCursorPosition(Width, i)
-            System.Console.Write("-")
-        Next
-    End Sub
-
-    Public Sub Input()
-        If Console.KeyAvailable Then
-            key = info.KeyChar
-        End If
-    End Sub
-
-    Public Sub DrawSnake()
-        System.Console.SetCursorPosition(x, y)
-        System.Console.Write("▀")
-    End Sub
-
-    Public Sub Position(x As Integer, y As Integer)
-        X_Position = x
-        Y_Position = y
     End Sub
 
 End Module
