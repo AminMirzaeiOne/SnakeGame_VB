5cf2e34669fccad562925d8d104d0a0299efbf1d
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 03:29:09 +0330

    - Default Value Width and Height - Create DrawBoard Function

--- SnakeGame/Board.vb
+++ SnakeGame/Board.vb
@@ -2,6 +2,32 @@
     Public Property Width As Integer
     Public Property Height As Integer
     Public Sub BoardConstructor()
+        Me.Width = 100
+        Me.Height = 50
 
+    End Sub
+
+    Public Sub DrawBoard()
+        System.Console.Clear()
+
+        For i As Integer = 0 To Width - 1
+            System.Console.SetCursorPosition(i, 0)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Width - 1
+            System.Console.SetCursorPosition(i, Height)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Height - 1
+            System.Console.SetCursorPosition(0, i)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Height - 1
+            System.Console.SetCursorPosition(Width, i)
+            System.Console.Write("-")
+        Next
     End Sub
 End Class
