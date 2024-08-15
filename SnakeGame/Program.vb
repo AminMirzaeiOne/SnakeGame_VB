4b8740df2998ff312447cb9d57be7718951b289c
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 02:41:58 +0330

    - Create Draw Board Function - Create Boolean Variable finished

--- SnakeGame/Program.vb
+++ SnakeGame/Program.vb
@@ -5,11 +5,38 @@ Imports System.Threading
 Imports System.Threading.Tasks
 
 Public Module Program
-    Public Property Widht As Integer
+    Public Property Width As Integer
     Public Property Height As Integer
     Public Sub Main()
-        Widht = 100
+        Dim finished As Boolean = False
+        Width = 100
         Height = 100
-
+        DrawBoard()
+        System.Console.Read()
     End Sub
+
+    Public Sub DrawBoard()
+        System.Console.Clear()
+
+        For i As Integer = 0 To Width - 1
+            System.Console.SetCursorPosition(i, 0)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Width - 1
+            System.Console.SetCursorPosition(i, Height)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Height - 1
+            System.Console.SetCursorPosition(0, i)
+            System.Console.Write("-")
+        Next
+
+        For i As Integer = 0 To Height - 1
+            System.Console.SetCursorPosition(Width, i)
+            System.Console.Write("-")
+        Next
+    End Sub
+
 End Module
