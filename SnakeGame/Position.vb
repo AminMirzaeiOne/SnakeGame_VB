464c799d69c87bea25c8355c762c5fccd7186dca
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 15:28:35 +0330

    - Create Position Class - Create x and y Property - Create Position Constructor

--- SnakeGame/Position.vb
+++ SnakeGame/Position.vb
@@ -1,3 +1,9 @@
 ﻿Public Class Position
+    Public Property x As Integer
+    Public Property y As Integer
 
+    Public Sub PositionConstructor(X As Integer, Y As Integer)
+        Me.x = X
+        Me.y = Y
+    End Sub
 End Class
