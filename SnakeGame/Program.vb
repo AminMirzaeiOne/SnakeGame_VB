36e7cc69736bd0df3b3649cb4f8d1b3b0b0ba5a9
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 03:49:32 +0330

    Call the Board class in Program - Main

--- SnakeGame/Program.vb
+++ SnakeGame/Program.vb
@@ -7,7 +7,13 @@ Imports System.Threading.Tasks
 Public Module Program
 
     Public Sub Main()
-
+        Dim finished As Boolean = False
+        Dim board As Board = New Board()
+        board.BoardConstructor()
+        While Not finished
+            board.DrawBoard()
+            System.Console.Read()
+        End While
     End Sub
 
 End Module
