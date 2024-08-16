592d3e17fa0a091f52cc5ce872ffa987b5776e54
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 15:14:18 +0330

    - Create Input Function

--- SnakeGame/Snake.vb
+++ SnakeGame/Snake.vb
@@ -1,4 +1,10 @@
 ﻿Public Class Snake
     Public keyInfo As System.ConsoleKeyInfo
+    Public key As Char = "w"
 
+    Public Sub Input()
+        If System.Console.KeyAvailable Then
+            Me.key = Me.keyInfo.KeyChar
+        End If
+    End Sub
 End Class
