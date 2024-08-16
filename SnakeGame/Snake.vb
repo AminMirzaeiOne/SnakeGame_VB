2e5d897cb79458e9e639bd97767853ec22cecfe3
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 15:15:33 +0330

    - Create x and y Property

--- SnakeGame/Snake.vb
+++ SnakeGame/Snake.vb
@@ -1,9 +1,11 @@
 ﻿Public Class Snake
     Public keyInfo As System.ConsoleKeyInfo
     Public key As Char = "w"
+    Public Property x As Integer
+    Public Property y As Integer
 
     Public Sub Input()
-        If System.Console.KeyAvailable Then
+        If System.Console.KeyAvailable = True Then
             Me.key = Me.keyInfo.KeyChar
         End If
     End Sub
