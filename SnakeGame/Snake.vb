47b0e6f2a0d4bfaa64c14beea0c93621f809829e
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-16 15:17:08 +0330

    - Create Constructor Function - Default Value x and y Property

--- SnakeGame/Snake.vb
+++ SnakeGame/Snake.vb
@@ -4,6 +4,11 @@
     Public Property x As Integer
     Public Property y As Integer
 
+    Public Sub SnakeConstructor()
+        Me.x = 20
+        Me.y = 20
+    End Sub
+
     Public Sub Input()
         If System.Console.KeyAvailable = True Then
             Me.key = Me.keyInfo.KeyChar
