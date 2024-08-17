a29cbbb44ef66cdbab3f5eeb7748fa36c2d062e7
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-17 04:18:48 +0330

    - Update Builder Food Class - Create DrawFood Function

--- SnakeGame/Food.vb
+++ SnakeGame/Food.vb
@@ -1,6 +1,14 @@
 ﻿Public Class Food
-    Public positon As SnakeGame.Position
+    Public foodPos As SnakeGame.Position = New SnakeGame.Position()
+    Private rnd As System.Random
+    Private board As SnakeGame.Board
+
     Public Sub New()
+        Me.foodPos.x = Me.rnd.Next(5, Me.board.Width)
+        Me.foodPos.y = Me.rnd.Next(5, Me.board.Height)
+    End Sub
+
+    Public Sub DrawFood()
 
     End Sub
 End Class
--- SnakeGame/Position.vb
+++ SnakeGame/Position.vb
@@ -2,6 +2,10 @@
     Public Property x As Integer
     Public Property y As Integer
 
+    Public Sub New()
+
+    End Sub
+
     Public Sub New(X As Integer, Y As Integer)
         Me.x = X
         Me.y = Y
