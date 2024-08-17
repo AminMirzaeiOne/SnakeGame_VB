48c4a31ebed352cfd4c35f7ab02757c6eb13f4e6
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-18 03:18:51 +0330

    - Update QuestionBoardSize Function

--- SnakeGame/Options.vb
+++ SnakeGame/Options.vb
@@ -6,12 +6,21 @@ Module Options
         Large = 100
     End Enum
 
-    Public ReadOnly Property BoardSize As SnakeGame.BoardSizes
-    Public ReadOnly Property CharBoard As Char = "-"
-    Public ReadOnly Property CharSnake As Char = "▀"
+    Public Property BoardSize As SnakeGame.BoardSizes
+    Public Property CharBoard As Char = "-"
+    Public Property CharSnake As Char = "▀"
 
     Public Sub QuestionBoardSize()
         System.Console.Write("What is the size of the game board? (Small , Medium , Large) : ")
+        Dim x As String = System.Console.ReadLine()
+        Select Case x
+            Case "small".ToLower()
+                BoardSize = BoardSizes.Small
+            Case "medium".ToLower()
+                BoardSize = BoardSizes.Medium
+            Case "large".ToLower()
+                BoardSize = BoardSizes.Large
+        End Select
     End Sub
 
 End Module
