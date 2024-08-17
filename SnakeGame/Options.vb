2c4cf940df7086245034a2d87327f44c95b8df64
Amin Mirzaei <aminmirzaei.og@gmail.com>
2024-08-18 03:11:51 +0330

    - Create BoardSizes Enum - Create BoardSize Property

--- SnakeGame/Options.vb
+++ SnakeGame/Options.vb
@@ -1,3 +1,12 @@
-﻿Module Options
+﻿
+Module Options
+    Public Enum BoardSizes
+        Small = 30
+        Medium = 50
+        Large = 100
+    End Enum
 
+
+
+    Public Property BoardSize As SnakeGame.BoardSizes
 End Module
